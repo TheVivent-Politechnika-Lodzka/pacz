e0a734746bb77b59ddad557000fb1691543b30f8
Adam Vivent Kapuściński <a.kapuscinski@thevivent.com>
2021-05-05 20:02:56 +0200

    nie wiem co robię

--- program liczący/CalcFile.py
+++ program liczący/CalcFile.py
@@ -2,6 +2,49 @@
 class CalcFile:
 
     FILE = None
+    LOC  = 0
+    NCNB = 0
+    EXEC = 0
+    COMMENTS = 0
 
     def __init__(self, path):
-        self.FILE = open(path, 'r')
+        self.FILE = open(path, 'r', encoding="utf8")
+        self.calc()
+
+
+    def calc(self):
+        wasPrevComm = False
+
+        for line in self.FILE:
+            self.LOC +=1
+            
+            # jeżeli pusta linia, leć dalej
+            if line == "": 
+                continue
+
+            # jeżeli komentarz, zanotuj i leć dalej
+            if line.startswith("//"):
+                self.COMMENTS += 1
+                continue
+
+            # obsługa komentarzy blokowych
+            if line.startswith("/*"):
+                wasPrevComm = True
+                self.COMMENTS += 1
+                continue
+
+            if wasPrevComm:
+                self.COMMENTS += 1
+                if line.endswith("*/"):
+                    wasPrevComm = False
+                continue
+
+            # komentarze i puste linie wykluczone, więc można
+            # zwiększyć licznik
+            self.NCNB += 1
+
+            # liczenie linii EXEC
+            if line.startswith(("for", "while", "{", "}", "if")):
+                continue
+
+            self.EXEC += 1
--- program liczący/main.py
+++ program liczący/main.py
@@ -1,4 +1,5 @@
 import os
+from CalcFile import CalcFile
 
 
 PATH = input("Podaj ścieżkę do projektu: ")
@@ -15,4 +16,41 @@ FILES = []
 for root, dirs, files in os.walk(PATH):
     for file in files:
         if file.endswith(tuple(EXT)):
-             print(os.path.join(root, file))
+            FILES.append(CalcFile(os.path.join(root, file)))
+            #  print(os.path.join(root, file))
+
+LOC  = 0
+NCNB = 0
+EXEC = 0
+COMMENTS = 0
+
+for f in FILES:
+    LOC += f.LOC 
+    NCNB += f.NCNB 
+    EXEC += f.EXEC 
+    COMMENTS += f.COMMENTS 
+
+#########################
+os.system()
+print("Z prezentacji:")
+print("LOC  [Lines of Code] - wszystkie linie")
+print("NCNB [Non-comment non-blank] - kod (bez komentarzy i pustych)")
+print("EXEC [Executable statements] - NCNB, bez ifów, forów itp (tylko linie które coś robią)")
+### tego jeszcze nie ma VVVVV
+print("CC   [Cyclomatic Complexity] - liczy się per metoda / nie chcę mi się myśleć jak")
+print("CP   [Comment percentage] - to w sumie prawie jest, tylko zrobić procenty")
+print("WMC  [Weighted method per class] - nwm")
+print("RFC  [Response for a class] - nwm")
+print("LCOM [Lack of cohesion of methods] - nwm")
+print("CBO  [Coupling between object classes] - nwm")
+print("DIT  [Depth of inheritance tree] - to chyba ręcznie się policzy")
+print("NOC  [Number of children] - to chyba też ręcznie")
+print()
+print("Własne:")
+print("COMM [Comments] - komentarze")
+
+
+print("LOC  - {}".format(LOC))
+print("NCNB - {}".format(NCNB))
+print("EXEC - {}".format(EXEC))
+print("COMM - {}".format(COMMENTS))
